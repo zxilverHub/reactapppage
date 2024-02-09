b149cc36c4d354f7dbd41dae9d5cff5432127a41
Silver Dave Ramos <zilvdaveramos@gmail.com>
2024-02-09 21:45:59 +0800

    cge nga try

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function App() {
+  return (
+    <div>
+      <h1>This is the react</h1>
+      <a href="https://zxilverhub.github.io/land/">Go to index</a>
+    </div>
+  )
+}
+
+export default App
